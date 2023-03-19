ca7f91ed72b9fa8125028ab80f43ecdc18db29ac
Orion <87782519+orion3005@users.noreply.github.com>
2023-03-19 00:04:57 -0700

    Update main.js
    Signed-off-by: Orion <87782519+orion3005@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 function dateTime() {
 	const date = new Date();
 	let today = date.toDateString();
-	let time = date.toLocaleTimeString();
+	let time = date.toLocaleTimeString({
+		hour12: false,
+	}
+	);
 	document.getElementById('date-time').innerHTML = '<p id="date">' + today + '</p><p id="time">' + time + '</p>';
 	setTimeout(dateTime, 1000);
 }
